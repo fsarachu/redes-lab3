33b3fb4e26e2211d9a21829512f4f5ebe918d061
Franco Sarachu <fsarachu@gmail.com>
2016-07-06 03:15:45 -0300

    Agrego chequeo de parámetros en cliente

--- src/Client.java
+++ src/Client.java
@@ -4,7 +4,15 @@ import java.io.InputStreamReader;
 import java.net.Socket;
 
 public class Client {
-    public static void main(String[] args) {
-        System.out.println("This is the client!");
+    public static void main(String[] args) throws IOException {
+
+        // Chequeo que haya pasado dos parámetros
+        if (args.length != 2) {
+            System.out.println("ERROR: pasar el host y puerto del servidor como argumentos. \n" +
+                    "Ejemplo: java Client localhost 1234");
+            System.exit(0);
+        }
+        
+
     }
 }
