d6a3397a52feba65a1c66211dee05ad531ab86ff
Franco Sarachu <fsarachu@gmail.com>
2016-07-06 03:40:57 -0300

    Implemento conexion básica del cliente con el servidor

--- src/Client.java
+++ src/Client.java
@@ -12,7 +12,23 @@ public class Client {
                     "Ejemplo: java Client localhost 1234");
             System.exit(0);
         }
-        
 
+        System.out.println("Connectando a " + args[0] + ":" + args[1] + " ...");
+        Socket client = new Socket(args[0], Integer.parseInt(args[1]));
+
+        try
+        {
+            System.out.println("Conección establecida con " + client.getRemoteSocketAddress() + "\n");
+//            OutputStream outToServer = client.getOutputStream();
+//            DataOutputStream out = new DataOutputStream(outToServer);
+//            out.writeUTF("Hello from "
+//                    + client.getLocalSocketAddress());
+//            InputStream inFromServer = client.getInputStream();
+//            DataInputStream in =
+//                    new DataInputStream(inFromServer);
+//            System.out.println("Server says " + in.readUTF());
+        } finally {
+            client.close();
+        }
     }
 }
